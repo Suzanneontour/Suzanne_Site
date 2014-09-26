70406463ce6afbebbf00dda34ce43062ef3c22e4
Suzanneontour <suzanneshrekgast@gmail.com>
2014-09-26 19:18:28 -0400

    Final Traffic Light
    Final Traffic Light, September 26, 2014

--- Traffic_Light/JS/application.js
+++ Traffic_Light/JS/application.js
@@ -1,2 +1,18 @@
+ 
+function buttonClick(turnOnLight) {
+	var redElem = document.getElementById('red');
+	var orangeElem = document.getElementById('orange');
+	var greenElem = document.getElementById('green');
 
-alert("hello world")
+	redElem.style.backgroundColor = "white";
+	orangeElem.style.backgroundColor = "white";
+	greenElem.style.backgroundColor = "white";
+
+	if (turnOnLight == 'stop') {
+		redElem.style.backgroundColor = "red";
+	} else if (turnOnLight == 'caution') {
+		orangeElem.style.backgroundColor = "orange";
+	} else {
+		greenElem.style.backgroundColor = "green";
+	}
+}
