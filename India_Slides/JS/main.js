f316fef8c3f40cc17f2f5d0054ce67f517f701c8
Suzanneontour <suzanneshrekgast@gmail.com>
2014-10-05 13:06:20 -0400

    Sunday, Oct. 5, 2014 1pm

---
+++ India_Slides/JS/main.js
@@ -0,0 +1,17 @@
+var images = ["img/India_1.jpg", "img/India_2.jpg", "img/India_3.jpg", "img/India_4.jpg","img/India_5.jpg"];
+var currentImage = 0;
+
+function changePicture(photoIndex) {
+	document.getElementById("mainPhoto").setAttribute("src", images[0], images[1], images[2], images[3], images[4], images[5])
+}
+
+function next() {
+	document.getElementById("").setAttribute("src")
+
+}
+
+function previous() {
+
+}
+
+  
