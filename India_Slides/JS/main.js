d1636a635070e2d93f8ff24c1e2d44db601b4217
Suzanneontour <suzanneshrekgast@gmail.com>
2014-10-09 18:36:09 -0400

    India Slideshow Final
    Those damn next and back buttons

--- India_Slides/JS/main.js
+++ India_Slides/JS/main.js
@@ -2,17 +2,19 @@ var images = ["img/India_1.jpg", "img/India_2.jpg", "img/India_3.jpg", "img/Indi
 var currentImage = 0;
 
 function changePicture(photoIndex) {
-	document.getElementById("mainPhoto").setAttribute("src", images[0], images[1], images[2], images[3], images[4], images[5])
+	var mainImage = document.getElementById("mainPhoto");
+	mainImage.setAttribute("src", images[photoIndex])
 
 }
 
-function next(photoStrip) {
-	document.getElementById("next").setAttribute("src", images[1], images[2], images[3], images[4], images[5])
+function next(forward) {
+	var nextImage = document.getElementById("next");
+	nextImage.setAttribute("src", images[currentImage])
 
 }
 
 function previous() {
-
+	var previousImage = document.getElementById("back");
 }
 
   
---
+++ Rock_Paper_Scissors/JS/main.js
@@ -0,0 +1,29 @@
+var userChoice = "paper";
+var computerChoice = "rock";
+var winner;
+
+var userWins = ["rockscissors", "paperrock", "scissorspaper"];
+
+varChoices = choices[Math.random()*choices.length];
+
+function evaluate() {
+	var msg = "Computer Wins!";
+
+	if(userChoice==computerChoice) {
+		msg = 'it is a tie!';
+	} else {
+
+		var outcome = userChoice + computerChoice;
+		$.each(userWins, function(index, value) {
+
+			$.each(userWins, function(index, value) {
+				if (outcome==value){
+					msg= "You Win!"
+				}
+			});
+
+			return msg;
+		}
+		var outcome = evaluate();
+		alert(evaluate());
+
