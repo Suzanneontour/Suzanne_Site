29d14786994e518b7605182be260ac4b8443ab1d
Suzanneontour <suzanneshrekgast@gmail.com>
2014-10-23 19:37:48 -0400

    Extended Lab Session - 10/23/2014
    First commit - starting JQ

--- Small_Stories_Slideshow/JS/main.js
+++ Small_Stories_Slideshow/JS/main.js
@@ -1,17 +1,30 @@
 $(document).ready( function() {
 
-    $('.photoContainer').hover( function() {
-        $(this).find('.luck').fadeIn(300);
-    }, function() {
-        $(this).find('.luck').fadeOut(100);
-    });
-	
+	$('.photoContainer').hover( function() {
+	    $(this).find('.luck').fadeIn(300);
+	}, function() {
+	    $(this).find('.luck').fadeOut(100);
+	});
+
+
+	var mainImageText = $('.photoContainer p').text();
+	$('.main-text p').click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
-
-
-
-function doSomething(num) {
-	alert
-}
-
-check out: Window Event Attributes
