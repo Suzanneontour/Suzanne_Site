a99148907df15717b91dfd1ab5b81d0e0de8f47b
Suzanneontour <suzanneshrekgast@gmail.com>
2014-10-24 19:12:42 -0400

    Friday Night start - October 24, 2014
    First commit

--- Small_Stories_Slideshow/JS/main.js
+++ Small_Stories_Slideshow/JS/main.js
@@ -1,30 +1,73 @@
-$(document).ready( function() {
-
-	$('.photoContainer').hover( function() {
-	    $(this).find('.luck').fadeIn(300);
-	}, function() {
-	    $(this).find('.luck').fadeOut(100);
-	});
-
-
-	var mainImageText = $('.photoContainer p').text();
-	$('.main-text p').click()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  $(".photoContainer").click(function(){
+    var div=$(".main-image-container");
+    div.animate({height:'500px', width: '500px', opacity: 1},"slow");
+    $('.small').hide();
+    $('.large').show();
+    //div.animate({width:'300px',visibility: "visible"},"slow");
+    //div.animate({height:'100px',visibility:"visible"},"slow");
+    //div.animate({width:'100px',visibility: "visible"},"slow");
+  });
 });
+
+
+
+
+
+
+
+
+/*	$('main-image-container').click(function() {
+	$('main-image-container').css('visibility, visible');
+	});
+}*/
+
+/*var images = ["img/________.jpg", "img/________.jpg", "img/_______.jpg", "img/_______.jpg","img/_______.jpg"];
+var currentImage = 0;
+
+document getElementById('id_of_image_in_slider').onclick = function() {
+function enlarge() {
+	document.getElementByClassName(main-image-container) {
+		
+	}
+}
+
+
+
+
+    
+    var mainImageText = $('.photoContainer p').text();
+
+	var myelement = $('.main-image-container p').text(mainImageText);
+
+	console.log(myelement);
+
+$('main-image-container').click(function() {
+$('main-image-container').css('visibility, visible');
+}
+*/
+/*
+$(".main-image-container").click(function() {
+		$(".main-image-container").animate({
+			width: '+=40px'
+		}, 1000);
+	});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
